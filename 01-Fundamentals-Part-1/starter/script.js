cc00be85b29758b72a2eab09b944ca22415a9619
ndir0 <nadirahmed14@yahoo.co.uk>
2023-01-18 19:54:52 +0000

    Understanding operator precedence

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -41,7 +41,6 @@ let age = 30; // let is mutable
 age = 31;
 
 const birthYear = 1999; // const is immutable
-*/
 
 const now = 2037;
 const ageJonas = now - 1991;
@@ -65,3 +64,14 @@ console.log(ageSarah >= 20);
 
 const isFullAge = ageSarah >= 18;
 console.log(now - 1991 > now - 2018);
+*/
+
+let x, y;
+x = y = 25 - 10 - 5; // x = y = 10, x = 10, assignment is right to left
+console.log(x, y);
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, averageAge);
