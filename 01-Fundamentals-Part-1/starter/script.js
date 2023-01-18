bd22840a3d1065523e6faae0d09fa6fd75e19adf
ndir0 <nadirahmed14@yahoo.co.uk>
2023-01-18 16:15:34 +0000

    Understanding data types

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,4 @@
+/*
 let js = 'amazing';
 console.log(40 + 8 + 23 - 10);
 
@@ -15,3 +16,24 @@ let person = "Jonas";
 let PI = 3.141;
 let myFirstJob = "Coder";
 let mySecondJob = "Teacher";
+*/
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Jonas');
+
+javascriptIsFun = 'YES';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 2023;
+console.log(typeof year);
+
+console.log(typeof null);
