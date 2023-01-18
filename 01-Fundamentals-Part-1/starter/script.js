ac7ed664ee5004ff9afb237f522662ef3b898dbc
ndir0 <nadirahmed14@yahoo.co.uk>
2023-01-18 16:01:13 +0000

    Understanding values and variables

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,17 @@
 let js = 'amazing';
-if (js = 'amazing') alert('JavaScript is fun');
 console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Bob";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = "JM";
+let $function = 27;
+let person = "Jonas";
+let PI = 3.141;
+let myFirstJob = "Coder";
+let mySecondJob = "Teacher";
