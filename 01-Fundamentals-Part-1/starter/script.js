d0d1e3c3ba1313e383bd51f566430c60f7b0165c
ndir0 <nadirahmed14@yahoo.co.uk>
2023-01-29 18:47:15 +0000

    Understanding ternary operator

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -243,7 +243,6 @@ if ((averageDolphins > averageKoalas) && averageDolphins >= 100) {
 } else if ((averageDolphins === averageKoalas) && (averageDolphins >= 100) && (averageKoalas >= 100)) {
     console.log("Draw");
 }
-*/
 
 const day = 'wednesday';
 
@@ -266,3 +265,20 @@ switch (day) {
     default:
         console.log('not valid');
 }
+*/
+
+const age = 23;
+// age >= 18 ? console.log("I like to drink wine") : console.log("I like to drink water");
+
+const drink = age >= 18 ? 'wine' : 'water';
+console.log(drink);
+
+let drink2
+if (age >= 18) {
+    drink2 = 'wine';
+} else {
+    drink2 = 'water';
+}
+console.log(drink2);
+
+console.log(`I like to drink ${age >= 18 ? 'wine' : 'water'}`);
