604245ef2054a4f12eee0e46b737b03226718d8e
ndir0 <nadirahmed14@yahoo.co.uk>
2023-06-06 17:32:55 +0100

    Understanding functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,3 +1,4 @@
+/*
 'use strict';
 
 let hasDriversLicense = false;
@@ -8,3 +9,24 @@ if (hasDriversLicense) console.log('I can drive');
 
 // const interface = 'Audio';
 // const private = 123;
+*/
+
+function logger() {
+    console.log('My name is Jonas');
+}
+
+// calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const fruitJuice = fruitProcessor(2, 3);
+console.log(fruitJuice);
+
+const num = Number('23');
+console.log(num);
