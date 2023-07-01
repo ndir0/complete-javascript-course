ad7d3ea84fe33983928c2f2159a4a4f5a749f0b3
ndir0 <nadirahmed14@yahoo.co.uk>
2023-07-01 18:48:58 +0100

    Understanding function declarations/expressions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -9,7 +9,6 @@ if (hasDriversLicense) console.log('I can drive');
 
 // const interface = 'Audio';
 // const private = 123;
-*/
 
 function logger() {
     console.log('My name is Jonas');
@@ -30,3 +29,18 @@ console.log(fruitJuice);
 
 const num = Number('23');
 console.log(num);
+*/
+
+// function declaration
+function calcAge1(birthYear) {
+    return (2023 - birthYear);
+}
+const age1 = calcAge1(1991);
+
+// function expression
+const calcAge2 = function (birthYear) {
+    return (2023 - birthYear);
+}
+const age2 = calcAge2(1991);
+
+console.log(age1, age2);
